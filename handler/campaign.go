a95ff7c6c056ecedc1e9fcac44128925fc5ffee8
nurfiansyah06 <6502676-nurfiansyah06@users.noreply.gitlab.com>
2021-06-23 00:05:53 +0700

    add endpoint detail campaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -34,3 +34,25 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	response := helper.APIResponse("Success to get Campaigns",http.StatusOK,"Success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *campaignHandler) GetCampaign(c *gin.Context)  {
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail campaign",http.StatusBadRequest,"Error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaignDetail , err := h.service.GetCampaignByID(input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail campaign",http.StatusBadRequest,"Error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("Success to get Campaign Detail",http.StatusOK,"Success", campaign.FormatCampaignDetail(campaignDetail))
+	c.JSON(http.StatusOK, response)
+}
